e85228b35bcdb240fbadf1532fe3cec5f979ed00
Ajaykumar-2246 <131764062+Ajaykumar-2246@users.noreply.github.com>
2024-11-28 13:46:35 +0530

    my first git update

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -7,11 +7,7 @@ const Navbar = () => {
   return (
     <div className="main-block m-0 p-0 ">
       <nav className=" navbar d-flex shadow-sm w-100">
-      <img
-        src={navicon}
-        alt=""
-        className="Navicon"
-      />
+      <i className='fas fa-cloud-sun  Navicon' ></i>
       <header className="fw-bold heading">Weather App</header>
 
     </nav>
